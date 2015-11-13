baf843d113970662e4dfcae7861b6db4ab2c358f
Kevin Ramsunder <kevinramsunder5@gmail.com>
2015-11-13 17:51:37 -0500

    Removed timer test library

--- async.js
+++ async.js
@@ -1,12 +1,11 @@
 var async = require('async');
 var Bot = require('./bot');
-var Timer = require('timer-machine')
 
 ASYNC_TASK_LIMIT = 10;
 
 var queue = async.queue(function(task, callback) {
     console.log('touched');
-    
+
     var bot = new Bot.Bot();
     bot.submitSearch(task.inst, task.term, task.dept);
 
