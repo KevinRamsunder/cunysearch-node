2495cd52a67f34db98fde1dfce4164c792619a10
Kevin Ramsunder <kevinramsunder5@gmail.com>
2015-11-13 17:52:08 -0500

    Added sample requests for testing

--- server.js
+++ server.js
@@ -1,2 +1,6 @@
-var queue = require('./async.js');
-queue.push('sample_link');
+var queue = require('./async');
+
+queue.queue.push({inst: 'QNS01', term: '1162', dept: 'CSCI'});
+queue.queue.push({inst: 'QNS01', term: '1162', dept: 'MATH'});
+queue.queue.push({inst: 'QNS01', term: '1162', dept: 'ENGL'});
+queue.queue.push({inst: 'QNS01', term: '1162', dept: 'PHYS'});
