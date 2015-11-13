df8634890a5f774a9211ed82423f2ed8a759dead
Kevin Ramsunder <kevinramsunder5@gmail.com>
2015-11-13 17:51:22 -0500

    Added async functionality

--- async.js
+++ async.js
@@ -1,14 +1,19 @@
 var async = require('async');
-var Bot = require('./bot.js');
+var Bot = require('./bot');
+var Timer = require('timer-machine')
 
 ASYNC_TASK_LIMIT = 10;
 
 var queue = async.queue(function(task, callback) {
-    // block UI [loading indicator]
-    // do web processing
-    // return results
-    // unblock UI [paste results]
+    console.log('touched');
+    
+    var bot = new Bot.Bot();
+    bot.submitSearch(task.inst, task.term, task.dept);
+
     callback(task);
+
 }, ASYNC_TASK_LIMIT);
 
-module.exports = queue;
+module.exports = {
+    queue: queue
+}
