5549383a1e639e1f594fc59b5c9e8ead928ab6a4
Kevin Ramsunder <kevinramsunder5@gmail.com>
2015-12-23 20:31:17 -0500

    Added support for config file

--- node/bot.js
+++ node/bot.js
@@ -3,6 +3,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var FormData = require('form-data');
 var formTemplate = require('./form');
+var config = require('./config.js');
 
 var Bot = function() {
 
@@ -10,8 +11,8 @@ var Bot = function() {
 
 Bot.prototype.submitSearch = function(inst, term, dept) {
     var options = {
-        url: 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/GUEST/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL?PortalActualURL=https%3a%2f%2fhrsa.cunyfirst.cuny.edu%2fpsc%2fcnyhcprd%2fGUEST%2fHRMS%2fc%2fCOMMUNITY_ACCESS.CLASS_SEARCH.GBL&amp;PortalContentURL=https%3a%2f%2fhrsa.cunyfirst.cuny.edu%2fpsc%2fcnyhcprd%2fGUEST%2fHRMS%2fc%2fCOMMUNITY_ACCESS.CLASS_SEARCH.GBL&amp;PortalContentProvider=HRMS&amp;PortalCRefLabel=Class%20Search&amp;PortalRegistryName=GUEST&amp;PortalServletURI=https%3a%2f%2fhome.cunyfirst.cuny.edu%2fpsp%2fcnyepprd%2f&amp;PortalURI=https%3a%2f%2fhome.cunyfirst.cuny.edu%2fpsc%2fcnyepprd%2f&amp;PortalHostNode=ENTP&amp;NoCrumbs=yes',
-        headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'},
+        url: config.cuny.longUrl,
+        headers: config.cuny.headers,
         jar: request.jar()
     };
 
@@ -22,10 +23,10 @@ Bot.prototype.submitSearch = function(inst, term, dept) {
         } 
         
         var parsed = cheerio.load(body);
-        var key = parsed('form[name=\'win0\']>input[name=\'ICSID\']').val();
+        var key = parsed(config.html.key).val();
 
         var submit_options = {
-            url: 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/GUEST/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL',
+            url: config.cuny.shortUrl,
             form: qs.stringify(formTemplate.getTemplate(key, inst, term, dept)),
             headers: options.headers,
             jar: options.jar
