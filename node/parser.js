099fd056372ea6b6d36ecba684f8cda980574fcb
Kevin Ramsunder <kevinramsunder5@gmail.com>
2016-01-04 20:23:22 -0500

    Added title parser

--- node/parser.js
+++ node/parser.js
@@ -3,7 +3,19 @@ var cheerio = require('cheerio');
 function parseResultBody(body) {
     $ = cheerio.load(body);
 
-    console.log($('[id^=win0divGPSSR_CLSRSLT_WRK_GROUPBOX2]').length);
+    var headers = resultsToArray($, '[id^=win0divGPSSR_CLSRSLT_WRK_GROUPBOX2]');
+    console.log(headers);
+}
+
+function resultsToArray(body, parseString) {
+    var results = body(parseString);
+    var resultsArray = [];
+
+    results.each(function(i) {
+        resultsArray[i] = $(this).text().trim();
+    });
+
+    return resultsArray;
 }
 
 module.exports = {
