0840762f305faa0c24ade5edf7f7e4e3a8538277
Kevin Ramsunder <kevinramsunder5@gmail.com>
2016-01-04 20:44:34 -0500

    Changed parser html keys

--- node/parser.js
+++ node/parser.js
@@ -3,16 +3,14 @@ var cheerio = require('cheerio');
 function parseResultBody(body) {
     $ = cheerio.load(body);
 
-    var headings = resultsToArray($, '[id^=win0divGPSSR_CLSRSLT_WRK_GROUPBOX2]');
-    console.log(headings);
-
+    var headings = resultsToArray($, 'div[id^=win0divGPSSR_CLSRSLT_WRK_GROUPBOX2]');
     var classesPerHeadings = resultsToArray($, 'table[id^=\'ACE_$ICField104\']');
-    var nbr = resultsToArray($, '[id^=MTG_CLASS_NBR]');
-    var section = resultsToArray($, '[id^=MTG_CLASSNAME]');
-    var status = resultsToArray($, '[class^=SSSIMAGECENTER]');
-    var room = resultsToArray($, '[id^=MTG_ROOM]');
-    var time = resultsToArray($, '[id^=MTG_DAYTIME]');
-    var instructors = resultsToArray($, '[id^=MTG_INSTR]');
+    var nbr = resultsToArray($, 'a[id^=MTG_CLASS_NBR]');
+    var section = resultsToArray($, 'a[id^=MTG_CLASSNAME]');
+    var status = resultsToArray($, 'img[class^=SSSIMAGECENTER]');
+    var room = resultsToArray($, 'span[id^=MTG_ROOM]');
+    var time = resultsToArray($, 'span[id^=MTG_DAYTIME]');
+    var instructors = resultsToArray($, 'span[id^=MTG_INSTR]');
 }
 
 function resultsToArray(body, parseString) {
