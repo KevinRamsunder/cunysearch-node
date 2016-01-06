366667c68af78ef97afb2eb3bbb60c88530231ff
Kevin Ramsunder <KevinRamsunder5@gmail.com>
2016-01-06 11:15:14 -0500

    Added function bodies

--- node/structure.js
+++ node/structure.js
@@ -1,29 +1,28 @@
 var cheerio = require('cheerio');
 
 var Structure = function() {
-
+	var classHeadings = [];
 };
 
 Structure.prototype.createSectionHeader = function(chp) {
-    
+    classHeadings.push(chp);
 };
 
 Structure.prototype.addClassToHeader = function(position, section) {
-    
+    classHeadings[position].addClassToHeader(section);
 };
 
 Structure.prototype.getSectionHeaderCount = function() {
-    
+    return classHeadings.length;
 };
 
 Structure.prototype.getClassHeader = function(index) {
-    
+    return classHeadings[index];
 };
 
 Structure.prototype.getClassSection = function(index1, index2) {
-    
+    return classHeadings[index1].classes[index2];
 };
-
 
 module.exports = {
     Structure: Structure
