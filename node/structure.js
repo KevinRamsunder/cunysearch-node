cab91b02e85fb9bafd3c8acbabaccb5bc9837a93
Kevin Ramsunder <KevinRamsunder5@gmail.com>
2016-01-06 11:23:19 -0500

    Fix function definitions

--- node/structure.js
+++ node/structure.js
@@ -1,27 +1,27 @@
 var cheerio = require('cheerio');
 
 var Structure = function() {
-	var classHeadings = [];
+	this.classHeadings = [];
 };
 
 Structure.prototype.createSectionHeader = function(chp) {
-    classHeadings.push(chp);
+    this.classHeadings.push(chp);
 };
 
 Structure.prototype.addClassToHeader = function(position, section) {
-    classHeadings[position].addClassToHeader(section);
+    this.classHeadings[position].addClassToHeader(section);
 };
 
 Structure.prototype.getSectionHeaderCount = function() {
-    return classHeadings.length;
+    return this.classHeadings.length;
 };
 
 Structure.prototype.getClassHeader = function(index) {
-    return classHeadings[index];
+    return this.classHeadings[index];
 };
 
 Structure.prototype.getClassSection = function(index1, index2) {
-    return classHeadings[index1].classes[index2];
+    return this.classHeadings[index1].classes[index2];
 };
 
 module.exports = {
