086cff5452408b342504d519e3a4b685b51760b5
Kevin Ramsunder <kevinramsunder5@gmail.com>
2016-01-05 20:47:59 -0500

    Added class structure function prototypes

---
+++ node/structure.js
@@ -0,0 +1,30 @@
+var cheerio = require('cheerio');
+
+var Structure = function() {
+
+};
+
+Structure.prototype.createSectionHeader = function(chp) {
+    
+};
+
+Structure.prototype.addClassToHeader = function(position, section) {
+    
+};
+
+Structure.prototype.getSectionHeaderCount = function() {
+    
+};
+
+Structure.prototype.getClassHeader = function(index) {
+    
+};
+
+Structure.prototype.getClassSection = function(index1, index2) {
+    
+};
+
+
+module.exports = {
+    Structure: Structure
+}
