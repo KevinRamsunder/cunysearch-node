a7cc976ef756627813aa08a194e202677bda2036
Kevin Ramsunder <KevinRamsunder5@gmail.com>
2016-01-20 16:50:08 -0500

    Started redis implementation

--- node/server/server.js
+++ node/server/server.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
+// require session store
+var redis = require('redis');
+var client = redis.createClient();
+var RedisStore = require('connect-redis')(session);
+
+// configuration strings
+var config = require('./config.js');
 
 // initialize express app
 var app = express();
+
+// redis client error handler
+client.on('error', function(err) {
+    console.log('Redis Error: ' + err)
+});
+
+// initialize session management
+app.use(session({
+    secret: config.server.session_secret,
+    saveUnitialized: true,
+    resave: true,
+    store: new RedisStore({host: 'localhost', port: '8000'})
+}));
+
+app.use(function printSession(req, res, next) {
+    console.log('Express Session: ' + req.session);
+    return next();
+});
 
 // connect public directory to node app
 app.use(express.static('../../public'));
