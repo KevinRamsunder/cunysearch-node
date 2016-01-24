0341fc953d86ae827afd57b432e9b64f7f40cd28
Kevin Ramsunder <kevinramsunder5@gmail.com>
2016-01-24 16:26:54 -0500

    Modified front-end to reflect changing inst, terms, and dept

--- public/app/controllers/MainController.js
+++ public/app/controllers/MainController.js
@@ -3,8 +3,8 @@ cunyapp.controller('MainController', mainController);
 // inject dependencies
 mainController.$inject = ['$scope', '$http', 'HttpPromise', 'JsonToTable', '$location'];
 
+// function to start http request
 cunyapp.factory('HttpPromise', function($http) {
-    // function to start http request
     return {
         getData: function getData() {
             return $http.get('api/search-request').then(function(response) {
@@ -17,17 +17,61 @@ cunyapp.factory('HttpPromise', function($http) {
 
 function mainController($scope, $http, HttpPromise, JsonToTable) {
     var self = this;
-    self.institutions = [];
 
+    // selected values for each input box
+    self.instSelected = undefined;
+    self.termSelected = undefined;
+    self.deptSelected = undefined;
+
+    // select bar data
+    self.institutions = [];
+    self.terms = [];
+    self.depts = [];
+
+    // array for school index retrieval
+    self.instKeys = [];
+
+    // get institutions, terms, and departments
     $http.get('info/inst').then(function(response) {
-        var institutions = response.data[0]; // list of insts
-        var instList = response.data[1]; // mongo doc object
+        self.instList = response.data[1]; // mongo doc object
+        self.institutionList = response.data[0]; // list of insts
         
-        for(var i = 0; i < institutions.length; i++) {
-            self.institutions.push(institutions[i].inst);
+        for(var i = 0; i < self.institutionList.length; i++) {
+            self.institutions.push(self.institutionList[i].inst);
+            self.instKeys.push(self.institutionList[i].index);
         }
     });
 
+    // process on change event for institution select bar
+    self.processInstitutionChange = function() {
+        var selectedIndex = self.institutions.indexOf(self.instSelected);
+        var indexToInstList = self.instKeys[selectedIndex];
+
+        self.updateTerms(indexToInstList);
+        self.updateDepts(indexToInstList);
+    };
+
+    self.updateTerms = function(indexToInstList) {
+        self.terms = [];
+        self.termSelected = undefined;
+        termList = self.instList[indexToInstList].term;
+
+        for(var i = 0; i < termList.length; i++) {
+            self.terms.push(termList[i].Name);
+        }
+    }
+
+    self.updateDepts = function(indexToInstList) {
+        self.depts = [];
+        self.deptSelected = undefined;
+        deptList = self.instList[indexToInstList].department;
+
+        for(var i = 0; i < deptList.length; i++) {
+            self.depts.push(deptList[i].Name);
+        }
+    }
+
+    // post search request to server
     self.postResults = function() {
         // construct promise
         var promise = HttpPromise.getData();
