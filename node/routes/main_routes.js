628cec3b315712efeb95a9321e79fec9d5e3fe4c
Kevin Ramsunder <kevinramsunder5@gmail.com>
2016-01-30 20:47:50 -0500

    Added seat information API endpoint

--- node/routes/main_routes.js
+++ node/routes/main_routes.js
@@ -2,6 +2,7 @@ var queue = require('../server/async');
 var express = require('express');
 var bodyParser = require('body-parser');
 var colors = require('colors');
+var Bot = require('../server/bot');
 
 // create routes for handling http requests
 var main_routes = express.Router();
@@ -11,14 +12,9 @@ main_routes.use(bodyParser.json());
 
 // get search results from cuny and return to front-end
 main_routes.post('/search-request', function(req, res) {
-    if(req.session.init === undefined) {
-        req.session.init = req.session.id;
-        console.log(('Session created @ ' + req.session.id).yellow);
-    } else {
-        console.log(('Return to session @ ' + req.session.id).yellow);
-    }
+    req.session.bot = new Bot();
 
-    console.log("Received request at /search-request");
+    handleSession(req);
 
     var data = {
         inst: req.body.data.inst.htmlKey,
@@ -26,14 +22,60 @@ main_routes.post('/search-request', function(req, res) {
         dept: req.body.data.dept.htmlKey
     };
 
-    search(data, res);
+    req.session.inst = data.inst;
+    req.session.term = data.term;
+    req.session.dept = data.dept;
+
+    search(data, req, res);
 });
 
-function search(values, res) {
-    queue.push({inst: values.inst, term: values.term, dept: values.dept}, function(val) {
+function search(values, req, res) {
+    var params = {
+        bot: req.session.bot,
+        inst: values.inst,
+        term: values.term,
+        dept: values.dept
+    };
+
+    queue.push(params, function(val, newBot) {
         console.log(('Cuny search complete. result length: ' + val.length).green);
+        req.session.bot = newBot;
         res.json(val);
     });
+}
+
+main_routes.post('/enrollment-request', function(req, res) {
+    // create new bot
+    req.session.searchBot = new Bot();
+
+    // re-create exact search request
+    var params = {
+        bot: req.session.searchBot,
+        inst: req.session.inst,
+        term: req.session.term,
+        dept: req.session.dept,
+        section: req.body.data.seatKey,
+        nbr: req.body.data.classID
+    };
+
+    // submit search and return results
+    queue.push(params, function(val, newBot) {
+        console.log((val).red);
+
+        req.session.searchBot = newBot;
+        res.json(val);
+    });
+});
+
+function handleSession(req) {
+    console.log("Received request at /search-request");
+    
+    if(req.session.init === undefined) {
+        req.session.init = req.session.id;
+        console.log(('Session created @ ' + req.session.id).yellow);
+    } else {
+        console.log(('Return to session @ ' + req.session.id).yellow);
+    }
 }
 
 module.exports = main_routes;
