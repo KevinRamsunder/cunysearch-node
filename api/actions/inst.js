8e7eedcd6a7de270036dca7567bb2a9232ccf5f1
Kevin Ramsunder <kevinramsunder5@gmail.com>
2016-06-03 18:15:27 -0400

    Formatting garbage

--- api/actions/inst.js
+++ api/actions/inst.js
@@ -2,54 +2,55 @@ import mongoose from 'mongoose';
 import mongo from '../utils/mongo.js';
 
 function getDatabaseModel(callback) {
-    let institutions = undefined;
-    mongoose.model('School').find().lean().exec((err, instList) => {
+  let institutions = undefined;
+
+  mongoose.model('School').find().lean().exec((err, instList) => {
+    if(err) {
+      console.log(err);
+      callback(err);
+    } else if(instList.length === 0) {
+      mongo.populateDatabase(err => {
         if(err) {
-            console.log(err);
-            callback(err);
-        } else if(instList.length === 0) {
-            mongo.populateDatabase(err => {
-                if(err) {
-                    console.log('Unable to populate database');
-                    return;
-                } else {
-                    institutions = processModel(instList);
-                    callback([institutions, instList]);
-                }
-            });
+          console.log('Unable to populate database');
+          return;
         } else {
           institutions = processModel(instList);
           callback([institutions, instList]);
         }
-    });
+      });
+    } else {
+      institutions = processModel(instList);
+      callback([institutions, instList]);
+    }
+  });
 }
 
 function processModel(instList) {
-    let institutions = [];
+  let institutions = [];
 
-    for(let i = 0; i < instList.length; i++) {
-        const collegeName = instList[i].institution.Name;
-        const htmlKey = instList[i].institution.HtmlKey;
-        const index = i;
+  for(let i = 0; i < instList.length; i++) {
+    const collegeName = instList[i].institution.Name;
+    const htmlKey = instList[i].institution.HtmlKey;
+    const index = i;
 
-        institutions.push({inst: collegeName, htmlKey, index: i});
-    }
+    institutions.push({inst: collegeName, htmlKey, index: i});
+  }
 
-    return sortInstitutionList(institutions);
+  return sortInstitutionList(institutions);
 }
 
 function sortInstitutionList(list) {
-    list.sort((a, b) => {
-        if (a.inst > b.inst) {
-            return 1;
-        } else if(a.inst < b.inst) {
-            return -1;
-        } else {
-            return 0;
-        }
-    });
+  list.sort((a, b) => {
+    if (a.inst > b.inst) {
+      return 1;
+    } else if(a.inst < b.inst) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
 
-    return list;
+  return list;
 }
 
 export default function inst(req) {
