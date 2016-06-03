e5f413f6ee4e3a26ede82015c6cf0d9c210b930f
Kevin Ramsunder <kevinramsunder5@gmail.com>
2016-06-03 18:16:21 -0400

    Formatting more garbage indentation

--- api/utils/asyncQueue.js
+++ api/utils/asyncQueue.js
@@ -6,20 +6,20 @@ const ASYNC_TASK_LIMIT = 100;
 
 // async queue for parallel processing
 const queue = async.queue((task, callback) => {
-    // if queue is given an initialized bot, recreate the bot instead of creating a new one
-    let bot = !task.bot.key || !task.bot.submit_options ? new Bot() : new Bot(task.bot);
+  // if queue is given an initialized bot, recreate the bot instead of creating a new one
+  let bot = !task.bot.key || !task.bot.submit_options ? new Bot() : new Bot(task.bot);
 
-    // submit server connect, scrape and parse
-    bot.submitSearch(task.inst, task.term, task.dept, task.nbr, (val, botRef) => {
-        // if queue is given a class ID, submit am additional request to get the seat info
-        if(task.nbr) {
-            botRef.submitClass('MTG_CLASSNAME$0', (val, botRef) => {
-                callback(val, botRef);
-            });
-        } else {
-            callback(val, botRef);
-        }
-    });
+  // submit server connect, scrape and parse
+  bot.submitSearch(task.inst, task.term, task.dept, task.nbr, (val, botRef) => {
+    // if queue is given a class ID, submit am additional request to get the seat info
+    if(task.nbr) {
+      botRef.submitClass('MTG_CLASSNAME$0', (val, botRef) => {
+        callback(val, botRef);
+      });
+    } else {
+      callback(val, botRef);
+    }
+  });
 }, ASYNC_TASK_LIMIT);
 
 export default queue;
--- api/utils/bot.js
+++ api/utils/bot.js
@@ -9,80 +9,80 @@ import Parser from './parse-package/main-parser.js';
 import EnrollmentInfoParser from './parse-package/parsers/enrollment-info-parser.js';
 
 class Bot {
-    constructor() {
-        // if not init_bot param is provided
-        if(arguments.length === 0) {
-            this.key = undefined;
+  constructor() {
+    // if not init_bot param is provided
+    if(arguments.length === 0) {
+      this.key = undefined;
 
-            this.submit_options = {
-                url: config.cuny.shortUrl,
-                form: undefined,
-                headers: undefined,
-                jar: undefined
-            }
-        } else {
-            // if an init_bot param IS provided
-            this.key = arguments[0].key;
-            this.submit_options = arguments[0].submit_options;
-        }
+      this.submit_options = {
+        url: config.cuny.shortUrl,
+        form: undefined,
+        headers: undefined,
+        jar: undefined
+      }
+    } else {
+      // if an init_bot param IS provided
+      this.key = arguments[0].key;
+      this.submit_options = arguments[0].submit_options;
     }
+  }
 
-    // submit custom search to CUNYFirst server
-    submitSearch(inst, term, dept, nbr, callback) {
-        const self = this;
+  // submit custom search to CUNYFirst server
+  submitSearch(inst, term, dept, nbr, callback) {
+    const self = this;
 
-        const options = {
-            url: config.cuny.longUrl,
-            headers: config.cuny.headers,
-            jar: request.jar()
-        };
+    const options = {
+      url: config.cuny.longUrl,
+      headers: config.cuny.headers,
+      jar: request.jar()
+    };
 
-        request.post(options, (err, res, body) => {
-            if(err) {
-                console.error(err);
-                return;
-            }
+    request.post(options, (err, res, body) => {
+      if(err) {
+        console.error(err);
+        return;
+      }
 
-            const parsed = cheerio.load(body);
-            self.key = parsed(config.html.key).val();
+      const parsed = cheerio.load(body);
+      self.key = parsed(config.html.key).val();
 
-            self.submit_options.headers = options.headers;
-            self.submit_options.jar = options.jar;
+      self.submit_options.headers = options.headers;
+      self.submit_options.jar = options.jar;
 
-            self.submit_options.form = nbr === undefined ?
-                formTemplate.getTemplate(self.key, inst, term, dept) :
-                formTemplate.fullSeatTemplate(self.key, inst, term, dept, nbr);
+      self.submit_options.form = nbr === undefined ?
+        formTemplate.getTemplate(self.key, inst, term, dept) :
+        formTemplate.fullSeatTemplate(self.key, inst, term, dept, nbr);
 
-            request.post(self.submit_options, (err, res, body) => {
-                request.post(self.submit_options, (err, res, body) => {
-                    const parser = new Parser(body);
-                    let results = parser.getJSON();
-                    results = JSON.stringify(results, null, 2);
-                    callback(results, self);
-                });
-            });
-        });
-    }
-
-    // submit custom class to get seat information
-    submitClass(section, callback) {
-        const self = this;
-
-        self.submit_options.form = qs.stringify(formTemplate.enrollmentTemplate(self.key, section));
-
+      request.post(self.submit_options, (err, res, body) => {
         request.post(self.submit_options, (err, res, body) => {
-            const parser = new EnrollmentInfoParser(body);
-
-            let results = {
-                classCapacity: parser.classCapacity,
-                classTotal: parser.classTotal,
-                classAvailable: parser.classAvailable
-            };
-
-            results = JSON.stringify(results, null, 2);
-            callback(results, self);
+          const parser = new Parser(body);
+          let results = parser.getJSON();
+          results = JSON.stringify(results, null, 2);
+          callback(results, self);
         });
-    }
+      });
+    });
+  }
+
+  // submit custom class to get seat information
+  submitClass(section, callback) {
+    const self = this;
+
+    self.submit_options.form = qs.stringify(formTemplate.enrollmentTemplate(self.key, section));
+
+    request.post(self.submit_options, (err, res, body) => {
+      const parser = new EnrollmentInfoParser(body);
+
+      let results = {
+        classCapacity: parser.classCapacity,
+        classTotal: parser.classTotal,
+        classAvailable: parser.classAvailable
+      };
+
+      results = JSON.stringify(results, null, 2);
+      callback(results, self);
+    });
+  }
 }
 
 export default Bot;
