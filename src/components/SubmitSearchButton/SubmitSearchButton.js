c579a0c03fefab5de43a3e05987116f1a1be646a
Kevin Ramsunder <kevinramsunder5@gmail.com>
2016-06-04 04:17:36 -0400

    Start typeahead component integration

--- src/components/SubmitSearchButton/SubmitSearchButton.js
+++ src/components/SubmitSearchButton/SubmitSearchButton.js
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import Helmet from 'react-helmet';
 import * as searchActions from 'redux/modules/search';
+import Typeahead from 'react-bootstrap-typeahead';
 
 @connect(
   state => ({results: state.search.searchResult}),
@@ -9,6 +10,16 @@ import * as searchActions from 'redux/modules/search';
 export default class Login extends Component {
   static propTypes = {
     query: PropTypes.func
+  }
+
+  state = {
+    appIsMounted: false
+  }
+
+  componentDidMount() {
+    requestAnimationFrame(() => {
+      this.setState({ appIsMounted: true });
+    });
   }
 
   handleSubmit = (event) => {
@@ -20,17 +31,28 @@ export default class Login extends Component {
   }
 
   render() {
+    const {appIsMounted} = this.state;
     const styles = require('./SubmitSearchButton.scss');
     return (
-      <div className={styles + ' container'}>
-        <Helmet title="Submit Search"/>
-        <h1>Submit Search</h1>
-        <div>
-          <form className="login-form form-inline" onSubmit={this.handleSubmit}>
-            <button className="btn btn-success" onClick={this.handleSubmit}><i className="fa fa-sign-in"/>{' '}Log In
-            </button>
-          </form>
+      <div>
+      {(appIsMounted) &&
+        <div className={styles + ' container'}>
+          <Helmet title="Submit Search"/>
+          <h1>Submit Search</h1>
+          <div>
+            <form className="login-form form-inline" onSubmit={this.handleSubmit}>
+              <Typeahead
+                labelKey="name"
+                onChange={this._handleChange}
+                options={[ {id: 1, name: 'ny'}, {id: 2, name: 'nj'},
+                {id: 3, name: 'al'}, {id: 4, name: 'wy'}, {id: 5, name: 'dc'}]}
+              />
+              <button className="btn btn-success" onClick={this.handleSubmit}><i className="fa fa-sign-in"/>{' '}Log In
+              </button>
+            </form>
+          </div>
         </div>
+      }
       </div>
     );
   }
