29c74421558f23216893a1190fa044d2a2b04a76
Kevin Ramsunder <kevinramsunder5@gmail.com>
2016-06-07 01:29:59 -0400

    When no results presents, redirect table to index

--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,22 @@
 import React from 'react';
 import {IndexRoute, Route} from 'react-router';
-import { isLoaded as isAuthLoaded, load as loadAuth } from 'redux/modules/auth';
 import {
     App,
-    Chat,
-    Home,
-    Widgets,
-    About,
-    Login,
-    LoginSuccess,
-    Survey,
     NotFound,
     SubmitSearchButton,
     Table
   } from 'containers';
 
 export default (store) => {
-  const requireLogin = (nextState, replace, cb) => {
-    function checkAuth() {
-      const { auth: { user }} = store.getState();
-      if (!user) {
-        // oops, not logged in, so can't be here!
-        replace('/');
-      }
-      cb();
+  const requireTableResult = (nextState, replace, cb) => {
+    const { search: { searchResult }} = store.getState();
+
+    // if no search results present, go back to search page
+    if (!searchResult) {
+      replace('/');
     }
 
-    if (!isAuthLoaded(store.getState())) {
-      store.dispatch(loadAuth()).then(checkAuth);
-    } else {
-      checkAuth();
-    }
+    cb();
   };
 
   /**
@@ -41,19 +27,9 @@ export default (store) => {
       { /* Home (main) route */ }
       <IndexRoute component={SubmitSearchButton}/>
 
-      { /* Routes requiring login */ }
-      <Route onEnter={requireLogin}>
-        <Route path="chat" component={Chat}/>
-        <Route path="loginSuccess" component={LoginSuccess}/>
+      <Route onEnter={requireTableResult}>
+        <Route path="table" component={Table}/>
       </Route>
-
-      { /* Routes */ }
-      <Route path="home" component={Home}/>
-      <Route path="about" component={About}/>
-      <Route path="login" component={Login}/>
-      <Route path="survey" component={Survey}/>
-      <Route path="widgets" component={Widgets}/>
-      <Route path="table" component={Table}/>
 
       { /* Catch all route */ }
       <Route path="*" component={NotFound} status={404} />
